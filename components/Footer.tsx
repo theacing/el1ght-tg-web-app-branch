fc548be16d6d7a66532087b303ee7aeed9941632
Kulunchick <nikolikrasovski@gmail.com>
2024-10-23 18:12:58 +0300

    hueta

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-
 const Footer = async () => {
      return (
-        <div className={'min-h-[290px] relative bg-neutral-800 mt-10 px-12'}>
-        <p className={'font-medium text-neutral-400 pt-14 text-lg'}>Sorry, but...</p>
-        <p className={'ml-5 font-medium text-neutral-400'}>That&apos;s all</p>
-
-            <Image className={'absolute bottom-2 right-0'} src={'./images/footerSpade.svg'} alt={'bg-image'} width={120} height={120} />
+        <div className={'min-h-[290px] bg-neutral-800 px-12'}>
+            <p className={'font-medium text-neutral-400 pt-14 text-lg'}>Sorry, but...</p>
+            <p className={'ml-5 font-medium text-neutral-400'}>That&apos;s all</p>
+            {/*<Image className={'absolute bottom-2 right-0'} src={'./images/footerSpade.svg'} alt={'bg-image'} width={120} height={120} />*/}
         </div>
      )
 }
